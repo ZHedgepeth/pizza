c47cf0f45e50fc253efabe4c9f9e439774650776
zhedgepeth <zhedgepeth1124@gmail.com>
2017-01-01 09:47:26 -0500

    fix syntax error in html capital sizes, else statement, and topping

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,37 @@
 
-function pizzaStore(pizzaSize, pizzaTopping) {
-  this.cost = 0
-  this.size = pizzaSize;
+function pizzaStore(size) {
+  this.cost = 0;
+  this.size = size;
   this.topping = [];
 }
 
 pizzaStore.prototype.finalPrice = function() {
   if (this.size === 'large') {
-    this.cost += 16.50 + (this.topping.length * 1)
+    this.cost += 16.50 + (this.topping.length)
   }
   else if (this.size === 'medium') {
-    this.cost += 14.50 + (this.topping.length * 1)
+    this.cost += 14.50 + (this.topping.length)
   }
   else if (this.size === 'small') {
-    this.cost += 12.50 + (this.topping.length * 1)
+    this.cost += 12.50 + (this.topping.length)
   }
-  else if (this.size != 'large', 'medium', 'small') {
+  else {
     alert("Enter a size if you want to eat!");
   }
 }
 $(document).ready(function() {
-  $('#pizza').submit(function(event) {
+  $('form#pizza').submit(function(event) {
   event.preventDefault();
-  var toppingsCheckArray = [];
-    var sizeCheck
 
-  var newPizza = new pizzaStore(toppingCheckArray, sizeCheck);
-
-  var sizeCheck = $("input:checkbox[name='size']:checked").each(function() {
-    newPizza.size.push($(this).val());
-  })
+  var sizeCheck = $("input:checkbox[name='size']:checked").val();
+  var newPizza = new pizzaStore(sizeCheck);
 
   var toppingsCheckArray = [];
    $("input:checkbox[name='topping']:checked").each(function() {
     newPizza.topping.push($(this).val());
   })
-
-
-  $("").text('stringtest');
+  newPizza.finalPrice();
+  $("#order").text ("TOTAL:" + newPizza.cost)
 
   });
 });
